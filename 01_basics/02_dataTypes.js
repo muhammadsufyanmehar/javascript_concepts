37c2d8553c222941b7a89ed4746a0364a5356e04
Muhammad Sufyan <chaudhry.muhammad.sufyan@gmail.com>
2025-02-18 21:03:56 +0500

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+let name = "Sufyan";
+let age = 20;
+let isApproved = true;
+let firstName; // undefined variable because it is not assigned any value.
+let lastName = null; // null variable because it is assigned null value.
+
+null; // null is an standalone value that represents the absence of an object value.
+undefined; // undefined is a type itself that represents the absence of a value. Means not assigned any value yet.
+
+// number => 2 to the power of 53 - 1
+// BigInt => 2 to the power of 53
+// string => "Hello World"
+// boolean => true or false
+// undefined => undefined
+// null => null
+// symbol => unique value that's not equal to any other value
+
+// objects
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+
+console.log(typeof undefined); // undefined type is undefined.
+console.log(typeof null); // null is an object type.
